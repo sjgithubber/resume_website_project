b820969b940681ff05194f669af2f734e2a5b1bd
sjgithubber <shubham991jain@gmail.com>
2025-03-19 21:25:44 +0530

    created repo

---
+++ models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Experience(models.Model):
+    pass
+
+class Projects(models.Model):
+    pass
+
+class Education(models.Model):
+    pass
+
+class Skills(models.Model):
+    pass
+
+class Certificates(models.Model):
+    pass
