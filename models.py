fd0fe700498910c7b4fa2302a6ad1c7f7033e4f5
sjgithubber <shubham991jain@gmail.com>
2025-03-20 19:57:39 +0530

    added mysql docker support

--- models.py
+++ models.py
@@ -2,16 +2,39 @@ from django.db import models
 
 # Create your models here.
 class Experience(models.Model):
+    years = 0
     pass
 
 class Projects(models.Model):
+    project1 = "website project"
+    project2 = "data visualization using streamlit"
+    project3 = "data analysis and visualization using pandas and seaborn"
+    project4 = "website project 2"
+    project5 = "website project 3"
     pass
 
 class Education(models.Model):
     pass
 
 class Skills(models.Model):
+    skill1 = "python"
+    skill2 = "django"
+    skill3 = "flask"
+    skill4 = "mysql or nosql or some other database"
+    skill5 = "pandas"
+    skill6 = "matplotlib, seaborn"
+    skill7 = "machineLearning"
     pass
 
 class Certificates(models.Model):
+
+    pass
+
+
+class BasicData(models.Model):
+    name = ""
+    address = ""
+    contact = ""
+    email = ""
+    description = ""
     pass
--- resume_website/settings.py
+++ resume_website/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = "resume_website.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#changes made here
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        'ENGINE': "django.db.backends.mysql",
+        'NAME': "django_db",
+        'USER': "django_user",
+        'PASSWORD': "passwd",
+        # 'HOST': "localhost", the localhost causes to use socket but with this it is tcp
+        'HOST':'127.0.0.1',
+        'PORT': "3306",
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
